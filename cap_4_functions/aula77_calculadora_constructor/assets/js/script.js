b6d1b46696d23a4730e9ff70a5c2396ac23422f1
Pedro Aires <1212436@sga.pucminas.br>
2022-01-19 16:02:48 -0300

    calculadoras com factory e constructor

---
+++ cap_4_functions/aula77_calculadora_constructor/assets/js/script.js
@@ -0,0 +1,69 @@
+// função construtora
+
+function Calculadora (){
+        const display = document.querySelector('.display');
+
+        this.inicia = () => {
+            clickBotoes();
+            pressionaEnter();
+        };
+
+        const pressionaEnter = () => {
+            display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    realizaConta();
+                }
+            });
+        };
+
+        const limparDisplay = () => {
+            display.value = '';
+        };
+
+        const apagaUm = () => {
+            display.value = display.value.slice(0, -1);
+        };
+
+        const realizaConta = () => {
+            let conta = display.value;
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('conta inválida!');
+                    return;
+                }
+                display.value = String(conta);
+            } catch (e) {
+                alert('conta inválida!');
+                return;
+            }
+        };
+
+        const clickBotoes = () => {
+            document.addEventListener('click', e => {
+                const el = e.target;
+                if (el.classList.contains('btn-num')) {
+                    btnParaDisplay(el.innerText);
+                }
+                if (el.classList.contains('btn-clear')) {
+                    limparDisplay();
+                }
+                if (el.classList.contains('btn-del')) {
+                    apagaUm();
+                }
+                if (el.classList.contains('btn-eq')) {
+                    realizaConta();
+                }
+            });
+        };
+
+        const btnParaDisplay = (valor) => {
+            display.value += valor;
+        };
+}
+
+const calculadora =  new Calculadora();
+calculadora.inicia();
+calculadora.clickBotoes();
